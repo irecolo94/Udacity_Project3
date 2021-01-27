cdd4d4570af3f138f6e684a1e05e4793bad6fe86
irecolo94 <ireblue@virgilio.it>
2021-01-27 23:00:58 +0100

    feat: client set up, can fetch from openweather

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -21,8 +21,38 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = 4000;
+const port = 4000; //process.env.PORT || 4000
 const server = app.listen(port, ()=> {
-  console.log("is it working?");
-  console.log(`running on localhost: {$port}`);
+  console.log('is it working?');
+  console.log(`running on localhost:${port}`)
+});
+
+// SET UP routes
+// get:
+
+app.get('/', (req, res)=> {
+  res.send(projectData);
+});
+
+// app.get('/api.openweathermap.org/data/2.5/weather?zip=85254&appid=6e30485752cf2a3013a66cca6d1829f6', (req, res) => {
+//   res.send(data);
+// });
+ //statt '/' das hier: baseURL+zip+key
+//
+// // post:
+// //
+// const data = [];
+
+app.post('/', (req, res)=> {
+  res.send('POST received')
+});
+
+app.post('/newPost', (req, res) => {
+  let data = req.body;
+  // data.push(req.body);
+  newData = {}
+  projectData['temperature'] = data.temperature;
+  projectData['date'] = data.date;
+  projectData['user response'] = data.userResponse;
+  console.log(projectData);
 });
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,52 @@
 /* Global Variables */
 
+// MANUELL POST:
+// const postData = async ( url = '', data = {})=>{
+//   console.log(data)
+//     const response = await fetch(url, {
+//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data), // body data type must match "Content-Type" header
+//   });
+//
+//     try {
+//       const newData = await response.json();
+//       return newData
+//     }catch(error) {
+//     console.log("error", error);
+//     }
+// };
+//
+// postData('/newPost', {temperature:'seagull', userResponse:'blabla', date:'culo'});
+
+// RETRIVE FROM OPENWEATHER
+
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=6e30485752cf2a3013a66cca6d1829f6';
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+const newZip =  document.getElementById('zip').value;
+getInfo(baseURL, newZip, apiKey)
+.then(function(data){console.log(data)
+});
+};
+
+const getInfo = async (baseURL, zip , key)=>{
+  const res = await fetch(baseURL+zip+key)
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
