db8e4f2352c86ac31cace75f4d29c93a494d360d
irecolo94 <ireblue@virgilio.it>
2021-01-30 18:50:37 +0100

    style: small change .css - app now responsive

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,42 +1,35 @@
-/* Global Variables */
-// Create a new date instance dynamically with JS
+// Global Variables
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-console.log(newDate);
-
-
-// RETRIVE FROM OPENWEATHER
 
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 let apiKey = '&appid=6e30485752cf2a3013a66cca6d1829f6';
 
+// RETRIVE FROM OPENWEATHER
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e){
 const newZip =  document.getElementById('zip').value;
 const newFeel = document.getElementById('feelings').value;
-console.log(newFeel);
 getInfo(baseURL, newZip, apiKey)
 .then(function(data){
-  console.log(data)
   postData('/addEntry', {temperature: data.main.temp, city: data.name, date: newDate, userresponse: newFeel});
 })
 .then(function(){updateUI()})
 }
 
-
+// GET data from given baseurl
 const getInfo = async (baseURL, zip , key)=>{
   const res = await fetch(baseURL+zip+key)
   try {
     const data = await res.json();
-    console.log(data.main.temp);
     return data;
   }  catch(error) {
     console.log("error", error);
-    // appropriately handle the error
   }
 }
 
+// POST the data
 const postData = async (url = '', data = {}) => {
   const response = await fetch(url, {
     method: 'POST',
@@ -53,13 +46,13 @@ const postData = async (url = '', data = {}) => {
   }catch(error) {console.log("error", error);}
 }
 
+// DYNAMICALLY UPDATE UI
 const updateUI = async () => {
   const request = await fetch('/all');
   try {
     const allData = await request.json();
-    console.log(allData);
     document.getElementById('date').innerHTML = 'date: ' + allData.date;
-    document.getElementById('city').innerHTML = 'your Zipcode corrisponds to: ' + allData.city; 
+    document.getElementById('city').innerHTML = 'your Zipcode corrisponds to: ' + allData.city;
     document.getElementById('temp').innerHTML = 'temperature: ' + allData.temperature;
     document.getElementById('content').innerHTML = 'your feeling: ' + allData.userresponse;
   }catch(error){
